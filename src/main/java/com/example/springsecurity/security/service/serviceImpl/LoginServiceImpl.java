e5d01d14931031e7ab5982e1189cd0f485e0ab38
“ALeo” <2923509522@qq.com>
2023-10-27 15:04:18 +0800

    test git 2.0

--- src/main/java/com/example/springsecurity/security/service/serviceImpl/LoginServiceImpl.java
+++ src/main/java/com/example/springsecurity/security/service/serviceImpl/LoginServiceImpl.java
@@ -1,21 +1,16 @@
 package com.example.springsecurity.security.service.serviceImpl;
 
-import com.example.springsecurity.pojo.LoginResponse;
 import com.example.springsecurity.pojo.LoginUser;
 import com.example.springsecurity.pojo.Response;
 import com.example.springsecurity.pojo.UserAuth;
 import com.example.springsecurity.security.SecurityConfig;
 import com.example.springsecurity.security.service.LoginService;
-import com.example.springsecurity.security.util.SecurityUtil;
 import com.example.springsecurity.util.jwt.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -48,7 +43,23 @@ public class LoginServiceImpl implements LoginService {
         map.put("token",jwt);
         return new Response(417,"欢迎回来,铁御",map);
     }
-    //authenticate存入redis
-    //redisCache.setCacheObject("login:"+userId,loginUser);
-    //把token响应给前端
+    //小綠點在哪裏？
+    public Response tologin(UserAuth user) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+//        securityConfig.passwordEncoder().encode(user.getPassword());
+        if(Objects.isNull(authenticate)){
+            throw new RuntimeException("任务代号4-1-7...登录失败");
+        }
+        //使用userid生成token
+        LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
+        String userId = loginUser.getUser().getId().toString();
+        String jwt = JwtUtil.createJWT(userId);
+        //authenticate存入redis
+        //redisCache.setCacheObject("login:"+userId,loginUser);
+        //把token响应给前端
+        HashMap<String,String> map = new HashMap<>();
+        map.put("token",jwt);
+        return new Response(417,"欢迎回来,铁御",map);
+    }
 }
