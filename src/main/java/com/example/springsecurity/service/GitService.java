d2c741ec06d1ab8dcb1f0e3c163e2aa0d291f7ed
“ALeo” <2923509522@qq.com>
2023-10-28 18:38:45 +0800

    git push test 2.0

--- src/main/java/com/example/springsecurity/service/GitService.java
+++ src/main/java/com/example/springsecurity/service/GitService.java
@@ -2,4 +2,5 @@ package com.example.springsecurity.service;
 
 public interface GitService {
     void gitTest();
+    void gitTest2();
 }
--- src/main/java/com/example/springsecurity/service/Impt/gitImplService.java
+++ src/main/java/com/example/springsecurity/service/Impt/gitImplService.java
@@ -9,4 +9,9 @@ public class gitImplService implements GitService {
     public void gitTest() {
         System.out.println("git updata test1.0");
     }
+    @Override
+    public void gitTest2() {
+        System.out.println("git updata test2.0");
+    }
+
 }
